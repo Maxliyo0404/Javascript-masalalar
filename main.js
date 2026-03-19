a1ed665010e8e2cb5e1d0fa50476efa024244eb8
Mahliyo <gasandigital13@gmail.com>
2026-03-19 22:14:59 +0500

    Javascript 8-masala-davomi

--- main.js
+++ main.js
@@ -463,9 +463,9 @@
 // console.log(color);
 
 // 8-masala
-// let nums = [5, 2, 9, 1];
-// let num = nums.sort()
-// console.log(num);
+let nums = [5, 2, 9, 1];
+let num = nums.sort()
+console.log(num);
 
 // let a = [1, 2];
 // let b =[3, 4];
