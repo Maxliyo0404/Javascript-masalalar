ffa3228299bb9258a0b988099def90f5c5a5998a
Mahliyo <gasandigital13@gmail.com>
2026-03-19 21:42:37 +0500

    Javascript

---
+++ main.js
@@ -0,0 +1,475 @@
+// var a=20;tin
+// var b=30;
+// c = a + b;
+
+ 
+
+//  let ism ="Gulxayo";
+//  ism ="malika";
+//  console.log(ism);
+
+//  let a = + prompt("a sonini topish");
+//  let b  = + prompt("b kiristish");
+//  c = a+b;
+//  console.log(c);
+ 
+
+//  let x = 123.2413;
+//  let y = 123.9;
+ 
+//  console.log( Math.floor(x) + Math.ceil(y));
+ 
+//   let number = 4567;
+//   let yaxlit = Math.floor(number / 10);
+//   let son = Math.floor(yaxlit % 11);
+
+ 
+//  console.log(son);
+ 
+//  let number = 45676;
+//  let a = Math. floor(number % 10);
+//  let c = Math. floor((number/ 100) % 10);
+//  let d = Math. floor((number/ 10) % 10);
+//  let e = Math. floor((number/ 10000) % 10);
+//  let b = Math. floor((number/ 1000) % 10);
+// //  console.log(a, b, c, d, e)
+// 4567
+// let number = 4567;
+// let a = Math. floor(number % 10);
+// let b = Math. floor((number / 10) % 10);
+// let c = Math. floor((number / 100) % 10);
+// let d = Math. floor(number / 1000);
+
+
+// // console.log(a + b + c + d);
+
+    // UY ISHI
+
+// (4)
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+// let c = prompt("c sonini kiriting");
+// if ((a <= b) && (b <= c)) {
+//     alert(" true")
+    
+// } else {
+//      alert(" false")
+//
+
+//  (5)
+//   let a = prompt("a sonini kiriting");
+//   let b = prompt("b sonini kiriting");
+//   if ((a % 2 !==0 && b % 2 === 0) ||  (a % 2 === 0 && a % 2 !== 0)) {
+//                alert("true")
+//   } else {
+//          alert("false")
+//   }
+
+// (6)
+//    let a = prompt("a sonini kiriting");
+//   let b = prompt("b sonini kiriting");
+//   if ((a % 2 !== 0) != (b % 2 !== 0)) {
+//                alert("true")
+//   } else {
+//          alert("false")
+//   }
+
+//  (7)
+//  let a = prompt(" a kriting");
+//  let b = prompt(" b kriting");
+//  let c = prompt(" c kriting");
+//  if ( (a !== b) && ( a !== c) ) {
+//                    alert("true")
+//  } else {
+//                    alert(" false")
+//  }
+// let summa = prompt(" pullarni kiriting")
+// let bilet = 5670;
+// let hotel = 2835;
+// let muzey = 1476;
+// let sum = bilet + hotel + muzey;
+// if (summa >= sum) {
+//     alert(" oq yo'l")
+// } else {
+//     alert(" sabr qiling")
+
+// let Madina = prompt("dars vaqtini kirgizdiring");
+
+// Madina > 12 ? alert("darsga kiring")  : alert(" kech qoldiz")
+
+// let  soat = prompt(" vaqtizni kiriting");
+// soat < 8 ? alert(" darsga kiring") : alert(" kech qoldiz");
+
+// // let soat = prompt(" vaqtizni kiriting ");
+// if (soat > 7 &&  soat < 8.30 ) {
+    
+// } else {
+    
+// }
+ 
+
+// let yosh = + prompt(" yoshingizni kiriting");
+
+// function( yosh);
+// if (yosh > 40 alert()) {
+    
+// } else {
+    
+
+// let a = + prompt(" a sonini kiriting");
+// let b = + prompt(" b sonini kiriting");
+//  let check = function(a, b){
+//     return a + b  
+    
+//  }
+//  console.log(check(12, 16));
+
+
+
+
+
+//  let sum = (a, b)=>{
+//      return a * b 
+//  }
+//   console. log(sum(12, 16));
+
+
+
+
+
+
+// let a = + prompt(" a sonini kiriting");
+// let b = + prompt(" b sonini kiriting");
+ 
+// let check = function(a, b){
+//     if (a > b) {
+//         alert("ture")
+//     } el.pse {
+//         alert("false")
+//     }
+// }
+// check(a, b)
+
+
+// 
+//    1 masala
+
+// function son(a, b) {
+//     return a+b
+// }
+// console.log(son(48, 29));
+
+    //   2 masala
+    // let   kvadrat = function(son){
+    //     return son * son
+    // }
+    // console.log( kvadrat(9));
+
+    // 3 masala
+   
+// let x =  +prompt("juft son kirgizdiring");
+// let y =  +prompt("toq son kirgizdiring");
+// S = x * y ;
+// function son( S){
+//     if (S % 2 === 0) {
+//         alert("juft son")
+//     } else {
+//         alert("toq son")
+//     }
+// }
+//  son( S);
+//   4 masala
+
+// let a = +prompt("birinchi sonni kiriting");
+// let b = +prompt("ikkinchi sonni kiriting");
+// let c = +prompt("uchunchi sonni kiriting");
+
+//  function Max(a, b, c){
+//     if (a >= b && a <= c || (b >= a && b >= c) || (c >= a && c >= b)) {
+//         alert("eng katta son")
+//     } else { 
+//         alert("eng kichik son")
+//     }
+//  }
+//  Max(a, b, c) ;
+
+// 5 masaltrgfcv 
+// let a = +prompt(" sonini kiriting");
+// function son(a){
+//   if (a % 5 === 0 &&  a % 3 ===0) {
+//     return("fizzbizz");
+
+//   } else if(a % 5 === 0){
+//     return("bizz");
+//   }
+//    else if(a % 3 === 0){
+//     return("fizz");
+//   }
+//    else {
+//     return("hech qanday son yo'q")
+//   }
+//   console.log(son(a));
+ 
+// }
+
+// let a = +prompt("a sonini kiriting");
+
+// function chek(a){
+//  if (a > 0) {
+//      alert("musbat son")
+//  } 
+//  else if(a === 0){
+//     alert("nol")
+//  }
+//  else {
+//     alert("manfiy son")
+//  }
+// }
+// chek(a);
+
+// 2-topshiriq
+// let a = +prompt("a sonini kiriting");
+// let b = +prompt("b sonini kiriting");
+// let c = +prompt("c sonini kiriting");
+
+// function average(a, b, c){
+//  katta = Math . max(a, b, c)
+//  kichik = Math . min(a, b,c)
+
+//  let h = (a + b + c) - (katta + kichik)
+//  console.log ( h );
+// }
+// average(a, b, c)
+
+
+// 3-masala
+// 1 declaration function
+
+// function checkname(names){
+//     if (names.length > 5) {
+//         return("Uzun ism")
+//     } else {
+//         return("Qisqa ism")
+//     }
+// }
+// console.log(checkname("Muhammadali"));   
+
+// 2- Expression function 
+
+// const names = prompt("ism yozing")
+// const checkname = function(names){
+//     if (names.length >= 6) {
+//         return("uzun ism")                                                                  
+//     } else {
+//         return("qisqa ism")
+//     }
+// }
+//  console.log(checkname(names));
+
+// 3-Arrow function  
+
+// let checkname = (names) => {
+//     if (names.length > 7) {
+//         return(" Uzun ism")
+//     } else {
+//         return("Qisqa ism")         
+//     }
+// }
+//  console.log(checkname("Laylo"));
+
+
+
+// 4- masala
+
+// 1 declaration function
+
+// function double(num){
+//     return num *2 ;
+// }                                                                                                                                                                                                            
+// console.log(double(5)); 
+
+// 2- Expression function 
+
+// let double  = function(num){                                                                     
+//     return num *2;           
+// }
+// console.log(double(8));  
+
+// 3-Arrow function  
+
+// let double = (num) =>{
+//     return num *2                          
+// }
+// console.log(double(20));  
+// 1-masala
+
+// let arr = [1, 2, 3, 4, 5];
+// let arr1 = arr.map ((el) => el  * 2);
+// console.log(arr1);
+  
+// 2-masala
+// let arr = [10, 15, 20, 25, 30];
+
+// let arr1 = arr.filter(el => el >    20);
+
+// console.log(arr1);
+
+// 3-masala
+
+// let arr = ["Ali", "Vali", "Hasan", "Husan"];
+
+// console.log(arr[2]);
+
+// 4-masala
+
+// let arr = [5, 8, 12, 20];
+
+// let arr1 = arr.reduce((acc, curr) => acc + curr, 0);
+// console.log(arr1);
+
+// 5-masala
+
+// // 1-masala
+//  const numbers = [20, 35, 99, 101, 45]
+//   const newNumbers = numbers.some(num => num > 100);
+//   console.log(newNumbers);
+  
+// 2-masala
+
+// const numbers = [3, 5, 7, 9, 1];
+// const newNumbers = numbers.every(num => num > 0)
+// console.log(newNumbers);
+
+
+// 3-masala
+
+// const numbers = [45, 12, 7, 30, 22];
+// const newNumbers = numbers.sort((a, b) => a-b );
+// console.log(newNumbers);
+
+// 4-masala
+
+// const react = ["HTML", "CSS", "Javascript", "React"];
+// const newReact = react.includes("React")
+// console.log(newReact);
+
+// 5-masala
+
+// const numbers = [2, 5, 8, 11, 14];
+// const newNumbers = numbers.filter(num => num % 2 === 0) .map(num => num * 3)
+// console.log(newNumbers);
+
+// 6-masala
+// const ball = [80, 90, 70, 60, 100];
+// const newBall = ball.reduce((sum, current)=> sum + current, 0);
+// const rezultat = newBall / ball.length;
+// console.log(rezultat);
+
+
+// let son = +prompt("son kirgizdiring");
+// if (son % 3 == 0 && son % 5 === 0) {
+//     alert("fizzbuzz chiqadi")
+// } else if (son % 3 === 0){
+//     alert("fizz")
+
+// } 
+//   else if (son % 5 === 0){
+//     alert("buzz")
+
+// } 
+// else {
+//     alert("false bo'lsin")
+// }
+// console.log(son);
+
+
+// 18-mart
+
+// 1-masala
+// let numbers = [10, 20, 30, 40];
+// let sum = numbers.length;
+//  console.log(sum);
+//   2-masala
+// let fruits = ["apple", "banana", "orange"];
+// fruits.pop()
+// console.log(fruits);
+//  3-masala
+// let cities = ["Tashkent", "Samarkand"];
+// cities.push(Fargona);
+// console.log(cities);
+
+// 4-masala
+//  let names = ["Ali", "Vali", "Hasan"];
+// for (let i = 0; i < names.length; i++){
+//     console.log(names[i]);
+    
+// }
+
+
+// 5-masala
+// let nums = [3, 9, 1, 12, 5];
+
+// let katta = nums.reduce((acc, curr,)=> Math.max(acc, curr) );
+
+// console.log(katta);
+
+
+
+// 
+
+
+// 1-masala
+
+// let nums = [3, 6, 8, 11, 14, 19];
+// let juft = nums.filter(num => num % 2 === 0);
+// console.log(juft);
+
+
+// 2-masala
+// let fruits = ["apple", "banana", "orange" ];
+// fruits.shift()
+// console.log(fruits);
+
+// 3-masala
+//  let cars = ["BMW","Audio"];
+//  cars.unshift("Damas")
+//  console.log(cars);
+
+// 4-MASALA
+
+// let words = ["Hello", "World"];
+// let word = words.join(" ")
+// console.log(word);
+
+// 5-masala
+
+// let nums = [3, 5, 12, 8];
+// let raqam = nums.some(function(num) {
+//     return num < 10;
+// })
+// console.log(raqam);
+// 6-masala
+// let nums = [4, 7, 9, -2];
+// let raqam = nums.every(function(num) {
+//     return num < -2;
+// })
+// console.log(raqam);
+
+// 7-masala
+// let colors = ["red", "green", "blue", "yellow"];
+// let color = colors.splice(1 , 1)
+// console.log(color);
+
+// 8-masala
+// let nums = [5, 2, 9, 1];
+// let num = nums.sort()
+// console.log(num);
+
+// let a = [1, 2];
+// let b =[3, 4];
+// let c = a.concat(b);
+// console.log(c);
+
+
+
+ 
